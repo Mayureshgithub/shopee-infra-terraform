0008b19205eaab0272da325d11b2b30c76155cfa
Mayureshgithub <105716638+Mayureshgithub@users.noreply.github.com>
2023-02-24 16:46:19 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.55.0"
+    }
+  }
+}
+
+
+# provider "aws" {
+#   region     = "us-east-1"
+#   access_key = "AKIA5O62AOFPUSICZGCJ"
+#   secret_key = "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"
+# }
